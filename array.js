974e2db1ecc72c19a5791060fa9152c702ac3b7f
Rizalfathi <rijalfahi303@gmail.com>
2024-01-29 13:31:43 +0700

    nambah fungsi array cuyy

---
+++ array.js
@@ -0,0 +1,8 @@
+// var namaarray = new Array()
+var loker = new Array("baju ✨", 10000, true)
+console.log(loker[1])
+console.log(loker[2])
+
+loker[3] = "ktp"
+loker.push("Cangcut")
+console.log(loker)
